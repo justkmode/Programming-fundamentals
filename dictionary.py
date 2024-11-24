cac2ba85e0618a47a7212381d6cc7604c4411f70
justkmode <136920389+justkmode@users.noreply.github.com>
2024-11-24 12:03:35 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -15,3 +15,18 @@ print(f"this is a {instance_id} instance")
 
 public_ip = ec2_instance.get("PublicIpAddress", "No Public IP address is here")
 print(f"the instances public ip is: {public_ip}")
+
+#Adding a new key-value pair
+ec2_instance["AvailablityZone"] = "eu-west-2"
+ec2_instance["State"] = "stopped"
+
+print(ec2_instance)
+
+# Using pop()
+rm_instance_type = ec2_instance.pop("InstanceType")
+print(f"removed instance type: {rm_instance_type}")
+print(ec2_instance)
+
+# Usign Del
+del ec2_instance["AvailablityZone"]
+print(ec2_instance)
